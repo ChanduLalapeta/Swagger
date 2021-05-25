ee76714fb4980b00bd86a3d624cd8d10fdf37c42
chandu.lalapeta <chandu.lalapeta@gmail.com>
2021-05-25 15:38:13 +0000

    Webapi with Swagger

---
+++ Swagger.Swashbuckle.webapi/Swagger.Swashbuckle.Webapi/Controllers/NewsBroadcastController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Swagger.Swashbuckle.Webapi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]    
+    public class NewsBroadcastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Crime Summary", "Sports Summary", "Entertainment Summary", "Politics Summary", "Weather Summary", "Elections Summary", "Travel Summary", "International Summary", "Kids Summary", "Strange Summary"
+        };
+
+        private static readonly string[] HeadLines = new[]
+        {
+            "Crime", "Sports", "Entertainment", "Politics", "Weather", "Elections", "Travel", "International", "Kids", "Strange"
+        };
+
+        private readonly ILogger<NewsBroadcastController> _logger;
+
+        public NewsBroadcastController(ILogger<NewsBroadcastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<NewsBroadcast> Get()
+        {          
+            return Enumerable.Range(0,10).Select(index => new NewsBroadcast
+            {
+                Date = DateTime.Now.AddDays(index),
+                HeadLine = HeadLines[index],
+                Summary = Summaries[index],
+                id = index + 1
+            })
+            .ToArray();
+        }
+
+        [HttpPost]
+        public NewsBroadcast Post(NewsBroadcast News)
+        {
+            return News;
+        }
+
+        
+    }
+}
---
+++ Swagger.Swashbuckle.webapi/Swagger.Swashbuckle.Webapi/NewsBroadcast.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Swagger.Swashbuckle.Webapi
+{
+    public class NewsBroadcast
+    {
+        public DateTime Date { get; set; }
+
+        public string HeadLine { get; set; }        
+
+        public string Summary { get; set; }
+
+        public int id { get; set; }
+    }
+}
